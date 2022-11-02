fa72ae92e53d3605099b87358290f4e0467935c9
Joshua <joshualeee2018@gmail.com>
2022-11-03 10:35:00 +1100

    Revert "basic progress for home page"
    This reverts commit df97ae07e0a8aa5c1c4c3c1ca91b7de7891bb5e0.

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,14 +1,14 @@
-// import React from 'react';
+import React from 'react';
 
-// const Button = (props) => {
-//   return <button
-//     style={{
-//       width: '3vw'
-//     }}
-//     onClick={props.click}
-//     >
-//       {props.text}
-//     </button>
-// }
+const Button = (props) => {
+  return <button
+    style={{
+      width: '3vw'
+    }}
+    onClick={props.click}
+    >
+      {props.text}
+    </button>
+}
 
-// export default Button;
+export default Button;
