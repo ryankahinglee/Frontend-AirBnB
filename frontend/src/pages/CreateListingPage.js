11f36fd8d10561c365e1062b3739ae93a92333ae
Joshua <joshualeee2018@gmail.com>
2022-11-11 18:06:30 +1100

    moved helper to file

--- frontend/src/pages/CreateListingPage.js
+++ frontend/src/pages/CreateListingPage.js
@@ -5,6 +5,7 @@ import makeRequest from '../makeRequest';
 import Bedroom from '../components/Bedroom';
 import { fileToDataUrl } from '../imageToURLHelper';
 import { jsonReader } from '../listingUploadHelper';
+import { validateJSON } from '../JSONValidateHelper';
 
 export default function CreateListing () {
   const navigate = useNavigate();
@@ -319,54 +320,4 @@ export default function CreateListing () {
     </div>
   </div>
   )
-}
-
-function validateJSON (result) {
-  if (!('title' in result) || typeof result.title !== 'string') {
-    return false;
-  }
-  if (!('address' in result) ||
-    result.address.constructor !== Object ||
-    !('streetDetails' in result.address) ||
-    !('city' in result.address) ||
-    !('state' in result.address) ||
-    !('postcode' in result.address) ||
-    !('country' in result.address) ||
-    typeof result.address.streetDetails !== 'string' ||
-    typeof result.address.city !== 'string' ||
-    typeof result.address.state !== 'string' ||
-    typeof result.address.postcode !== 'number' ||
-    typeof result.address.country !== 'string'
-  ) {
-    return false;
-  }
-  if (!('price' in result) || typeof result.price !== 'number') {
-    return false;
-  }
-  if (!('thumbnail' in result) || typeof result.thumbnail !== 'string') {
-    return false;
-  }
-  if (!('metadata' in result) ||
-    result.metadata.constructor !== Object ||
-    !('type' in result.metadata) ||
-    !('bathrooms' in result.metadata) ||
-    !('amenities' in result.metadata) ||
-    !('bedrooms' in result.metadata) ||
-    typeof result.metadata.type !== 'string' ||
-    typeof result.metadata.bathrooms !== 'number' ||
-    typeof result.metadata.amenities !== 'string' ||
-    !(Array.isArray(result.metadata.bedrooms))
-  ) {
-    return false;
-  } else {
-    for (const bedroom of result.metadata.bedrooms) {
-      if (!('roomType' in bedroom) ||
-      !('numBeds' in bedroom) ||
-      typeof bedroom.roomType !== 'string' ||
-      typeof bedroom.numBeds !== 'number') {
-        return false;
-      }
-    }
-  }
-  return true;
 }
